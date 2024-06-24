4dbecc61708ccf884ce7984a1d69a8569191ba0b
matt12171 <100318692+matt12171@users.noreply.github.com>
2024-06-24 22:38:00 +0100

    Showing bleet

--- apps/phone/src/apps/bleeter/components/pages/Replies.tsx
+++ apps/phone/src/apps/bleeter/components/pages/Replies.tsx
@@ -1,16 +1,20 @@
 import { useLocation } from 'react-router-dom';
 import React from 'react';
+import { useBleetsValue } from '@apps/bleeter/hooks/state';
+import { BleetItem } from './BleetItem';
 
 export const Replies: React.FC = () => {
-
-  // need to grab bleet by id and only grab bleets with the same repliedId
 
   const url = useLocation().pathname;
   const id = url.split("replies/")[1];  
 
+  const bleets = useBleetsValue();
+  const singleBleet = bleets.find(bleet => bleet.id === Number(id));
+
+
   return (
     <div>
-      Bleeter Reply: {id}
+      <BleetItem key={singleBleet.id} bleet={singleBleet}/>
     </div>
   );
 };
